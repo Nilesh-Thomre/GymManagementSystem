5ece3a74ecfcab7e98cfd9dc3eeefcedb2586d77
EC2 Default User <ec2-user@ip-172-31-36-102.eu-west-1.compute.internal>
2024-04-17 21:15:36 +0000

    test

--- gym_management/urls.py
+++ gym_management/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
 from django.shortcuts import redirect
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
    path('admin/', admin.site.urls),
     path('members/', include('members.urls')),
     path('', lambda request: redirect('member_list', permanent=False)),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
